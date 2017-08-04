767afaa9eede5e72b7af01c11c4dcf457020af37
Brendon Ryan <brendon.ryan@imc.com>
2017-08-04 16:16:40 +1000

    Default ls to exa

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,10 +1,14 @@
 function ls
-    set ls_bin (command -s ls)
-    switch (uname)
-        case Darwin
-            if test -e /usr/local/bin/gls
-                set ls_bin /usr/local/bin/gls
-            end
+    if type -q exa
+        exa $argv
+    else
+        set ls_bin (command -s ls)
+        switch (uname)
+            case Darwin
+                if test -e /usr/local/bin/gls
+                    set ls_bin /usr/local/bin/gls
+                end
+        end
+        eval (string escape -- $ls_bin --color=auto $argv)
     end
-    eval (string escape -- $ls_bin --color=auto $argv)
 end
