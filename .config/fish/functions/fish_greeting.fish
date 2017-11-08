731509f167a6adfe5665d4eaba4543751cf72003
Brendon Ryan <brendon.ryan@imc.com>
2017-11-08 17:09:59 +1100

    update j; add sakura squid localforward

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,4 +1,4 @@
 function fish_greeting
     set_color -o black
-	fortune -s computers humorists literature love people platitudes science wisdom work
+	fortune -s computers humorists literature love people platitudes science wisdom
 end
